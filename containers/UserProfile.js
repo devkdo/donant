e092ab615bfc591054a7d4d16e44a8cf3ba6069f
dekaido <kml5197@outlook.com>
2022-01-18 01:01:27 -0500

    #2 Adding folder structure and sample classes

---
+++ containers/UserProfile.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Image, StyleSheet } from 'react-native'
+
+export default function UserProfile() {
+  return (
+    <Image
+      style={styles.image}
+      source={{ uri: 'https://www.reactnative.express/static/logo.png' }}
+    />
+  )
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 200,
+    height: 200,
+  },
+})
