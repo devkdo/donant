3c7d974355b5c744b6b630561be7e5e56e65e119
dekaido <kml5197@outlook.com>
2022-01-23 13:42:58 -0500

    #2 wip app js

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Root = createStackNavigator();
+
+const Screen1 = ({ navigation, route }) => (
+	<View style={styles.screen}>
+		<Text style={styles.title}>Screen 1</Text>
+		<Button
+			title="Go to Screen 2"
+			onPress={() => {
+				navigation.push('Screen2')
+			}}
+		/>
+	</View>
+)
+const Screen2 = ({ navigation, route }) => (
+	<View style={styles.screen}>
+		<Text style={styles.title}>Screen 2</Text>
+		<Button
+			title="Go back"
+			onPress={() => {
+				navigation.pop()
+			}}
+		/>
+	</View>
+)
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+	<NavigationContainer>
+		<Root.Navigator>
+			<Root.Screen name="Screen1" component={Screen1} />
+			<Root.Screen name="Screen2" component={Screen2} />
+		</Root.Navigator>
+	</NavigationContainer>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
@@ -18,3 +52,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+// const styles = StyleSheet.create(
+// 	screen: {
+// 	  marginTop: 40,
+// 	  alignItems: 'center',
+// 	},
+// 	title: {
+// 	  padding: 20,
+// 	  fontSize: 42,
+// 	},
+//   })
