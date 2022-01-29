db1c85af3d46d960ad0c7636f3dd6073bdb5dd57
dekaido <kml5197@outlook.com>
2022-01-28 19:37:18 -0500

    #2 Login screen populates reactively

--- app/screens/Login.js
+++ app/screens/Login.js
@@ -1,24 +1,47 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 
-export default function Login() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello!</Text>
-    </View>
-  )
+function Login(props) {
+	return (
+		<ImageBackground
+			style={styles.background}
+			source={require("../assets/background.jpg")}
+		>
+			<View style={styles.logoContainer}>
+				<Image style={styles.logo} source={require('../assets/logo-red.png')}/>
+				<Text>Sell What You Don't Need</Text>
+			</View>
+			<View style={styles.loginButton}></View>
+			<View style={styles.registerButton}></View>
+		</ImageBackground>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    backgroundColor: 'whitesmoke',
-    color: '#4A90E2',
-    fontSize: 24,
-    padding: 10,
-  },
+	background: {
+		flex: 1,
+		justifyContent: "flex-end",
+		alignItems: 'center',
+	},
+	loginButton: {
+		width: "100%",
+		height: 70,
+		backgroundColor: "#fc5c65"
+	},
+	logo: {
+		width: 100,
+		height: 100,
+	},
+	logoContainer: {
+		position: "absolute",
+		top: 70,
+		alignItems: 'center',
+	},
+	registerButton: {
+		width: "100%",
+		height: 70,
+		backgroundColor: "#4ecdc4"
+	}
 })
+
+export default Login;
